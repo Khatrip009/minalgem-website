fdea6d03fbffc8b67388e4803630f4de1bed8a2a
Khatrip009 <Khatrip.009@gmail.com>
2026-05-13 17:41:10 +0530

    fdfd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/minalgems-jewellery/',   // 👈 replace with your actual repo name
+  base: '/',   // 👈 replace with your actual repo name
 })
